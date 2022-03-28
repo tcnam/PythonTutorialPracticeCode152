f9846cb47f7d2e67acb86a3fc3ccbe3bfbf01f46
Nam Tran <tcnam2000@gmail.com>
2022-03-28 22:19:28 +0700

    add auto generation SQLQUerries for KHOA in QLGV database from excel

---
+++ SQLInsertQuerries/QLGV/Khoa.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from pathlib import Path
+from datetime import datetime
+
+path=str(Path(__file__).resolve())
+path=path.replace('.py','.xlsx')
+print(path)
+
+data =pd.read_excel(path)
+df=pd.DataFrame(data)
+
+SQLQuerries=''
+
+for i in df.index:
+    makhoa=df.iloc[i][0]
+    tenkhoa=df.iloc[i][1]
+    ngtlap=df.iloc[i][2]
+    if type(ngtlap)==str:
+        ngtlap=datetime.strptime(ngtlap, '%d/%m/%Y')
+    print (ngtlap)
+    trgkhoa=df.iloc[i][3]
+    SQLQuerries=SQLQuerries + "insert into KHOA(MAKHOA, TENKHOA, NGTLAP, TRGKHOA) values ('{}','{}','{}','{}')\n".format(makhoa, tenkhoa, ngtlap, trgkhoa)
+
+print(SQLQuerries)
